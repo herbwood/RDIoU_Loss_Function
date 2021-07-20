295e7b1af68f94f39df032b1bb01c9cf3b9aa468
herbwood <herbwood27@gmail.com>
2021-07-20 21:17:31 +0900

    added matcher comments

--- _utils.py
+++ _utils.py
@@ -128,7 +128,7 @@ def encode_boxes(reference_boxes, proposals, weights):
     return targets
 
 
-class BoxCoder(object):
+class BoxCoder:
     """
     This class encodes and decodes a set of bounding boxes into
     the representation used for training the regressors.
@@ -171,11 +171,19 @@ class BoxCoder(object):
         # type: (Tensor, List[Tensor]) -> Tensor
         assert isinstance(boxes, (list, tuple))
         assert isinstance(rel_codes, torch.Tensor)
+
+        # get all the boxes in the batch
+        # and concat by row 
         boxes_per_image = [b.size(0) for b in boxes]
         concat_boxes = torch.cat(boxes, dim=0)
         box_sum = 0
+
+        # get total number of boxes 
         for val in boxes_per_image:
             box_sum += val
+
+        # if there is more than one bbox in the image
+        # decode the boxes 
         if box_sum > 0:
             rel_codes = rel_codes.reshape(box_sum, -1)
         pred_boxes = self.decode_single(
@@ -221,44 +229,46 @@ class BoxCoder(object):
         pred_boxes3 = pred_ctr_x + torch.tensor(0.5, dtype=pred_ctr_x.dtype, device=pred_w.device) * pred_w
         pred_boxes4 = pred_ctr_y + torch.tensor(0.5, dtype=pred_ctr_y.dtype, device=pred_h.device) * pred_h
         pred_boxes = torch.stack((pred_boxes1, pred_boxes2, pred_boxes3, pred_boxes4), dim=2).flatten(1)
+
         return pred_boxes
 
 
-class Matcher(object):
+class Matcher:
     """
     This class assigns to each predicted "element" (e.g., a box) a ground-truth
     element. Each predicted element will have exactly zero or one matches; each
     ground-truth element may be assigned to zero or more predicted elements.
+
     Matching is based on the MxN match_quality_matrix, that characterizes how well
     each (ground-truth, predicted)-pair match. For example, if the elements are
-    boxes, the matrix may contain box IoU overlap values.
+    boxes, the matrix may contain box IoU overlap values. 
+
     The matcher returns a tensor of size N containing the index of the ground-truth
     element m that matches to prediction n. If there is no match, a negative value
-    is returned.
+    is returned. 
     """
 
+    # class variable
     BELOW_LOW_THRESHOLD = -1
     BETWEEN_THRESHOLDS = -2
 
     __annotations__ = {
-        'BELOW_LOW_THRESHOLD': int,
-        'BETWEEN_THRESHOLDS': int,
+        'BELOW_LOW_THRESHOLD' : int,
+        'BETWEEN_THRESHOLD' : int
     }
 
-    def __init__(self, high_threshold, low_threshold, allow_low_quality_matches=False):
-        # type: (float, float, bool) -> None
+    def __init__(self, high_threshold:float, low_threshold:float, allow_low_quality_matches:bool = False):
         """
         Args:
-            high_threshold (float): quality values greater than or equal to
+            high_threshold (float) : quality values greater than or equal to 
                 this value are candidate matches.
-            low_threshold (float): a lower quality threshold used to stratify
+            low_threshold (float) : a lower quality threshold used to stratify
                 matches into three levels:
                 1) matches >= high_threshold
-                2) BETWEEN_THRESHOLDS matches in [low_threshold, high_threshold)
-                3) BELOW_LOW_THRESHOLD matches in [0, low_threshold)
-            allow_low_quality_matches (bool): if True, produce additional matches
-                for predictions that have only low-quality match candidates. See
-                set_low_quality_matches_ for more details.
+                2) BETWEEN_THESHOLD matches in [low_threshold, high_threshold]
+                3) BELOW_LOW_THRESHOLD matches in [0, low_threshold]
+            allow_low_quality_matches (bool) : if True, produce additional matches
+                for predictions that have only low-quality match candidates. 
         """
         self.BELOW_LOW_THRESHOLD = -1
         self.BETWEEN_THRESHOLDS = -2
@@ -267,30 +277,37 @@ class Matcher(object):
         self.low_threshold = low_threshold
         self.allow_low_quality_matches = allow_low_quality_matches
 
-    def __call__(self, match_quality_matrix):
+    def __call__(self, match_quality_matrix:Tensor[float]):
         """
         Args:
-            match_quality_matrix (Tensor[float]): an MxN tensor, containing the
-            pairwise quality between M ground-truth elements and N predicted elements.
+            match_quality_matrix (Tensor[float]) : an MxN tensor, containing the
+                pairwise quality between M ground-truth elements and N predicted elements.
+
         Returns:
-            matches (Tensor[int64]): an N tensor where N[i] is a matched gt in
-            [0, M - 1] or a negative value indicating that prediction i could not
-            be matched.
+            matches (Tensor[int64]) : an N tensor where N[i] is a matched gt in 
+                [0, M - 1] or a negative value indicating that prediction i could not 
+                be matched. 
         """
+
+        # if total number of match_quality_matrix is zero
         if match_quality_matrix.numel() == 0:
-            # empty targets or proposals not supported during training
+            # empty targets or proposals not supported during training 
             if match_quality_matrix.shape[0] == 0:
                 raise ValueError(
                     "No ground-truth boxes available for one of the images "
-                    "during training")
+                    "during training"
+                )
             else:
                 raise ValueError(
                     "No proposal boxes available for one of the images "
-                    "during training")
+                    "during training"
+                )
 
         # match_quality_matrix is M (gt) x N (predicted)
         # Max over gt elements (dim 0) to find best gt candidate for each prediction
-        matched_vals, matches = match_quality_matrix.max(dim=0)
+        # matched_vals : best matching values 
+        # matches : best matching value index 
+        matched_vals, matches = match_quality_matrix.max(0)
         if self.allow_low_quality_matches:
             all_matches = matches.clone()
         else:
@@ -301,6 +318,9 @@ class Matcher(object):
         between_thresholds = (matched_vals >= self.low_threshold) & (
             matched_vals < self.high_threshold
         )
+
+        # negative values : -1
+        # neutral values : -2
         matches[below_low_threshold] = self.BELOW_LOW_THRESHOLD
         matches[between_thresholds] = self.BETWEEN_THRESHOLDS
 
@@ -314,32 +334,23 @@ class Matcher(object):
         """
         Produce additional matches for predictions that have only low-quality matches.
         Specifically, for each ground-truth find the set of predictions that have
-        maximum overlap with it (including ties); for each prediction in that set, if
-        it is unmatched, then match it to the ground-truth with which it has the highest
-        quality value.
+        maximum overlap with it (including ties); for each prediction in that set, if 
+        it is unmatched, then match if to the ground-truth with which it has the highest
+        quality value. 
         """
-        # For each gt, find the prediction with which it has highest quality
+        # For each gt, find the prediction with which it has highest quality 
+        # highest_quality_foreach_gt : highest iou values 
         highest_quality_foreach_gt, _ = match_quality_matrix.max(dim=1)
         # Find highest quality match available, even if it is low, including ties
+        # tie means bbox having same iou value with different ground-truths
         gt_pred_pairs_of_highest_quality = torch.where(
             match_quality_matrix == highest_quality_foreach_gt[:, None]
         )
-        # Example gt_pred_pairs_of_highest_quality:
-        #   tensor([[    0, 39796],
-        #           [    1, 32055],
-        #           [    1, 32070],
-        #           [    2, 39190],
-        #           [    2, 40255],
-        #           [    3, 40390],
-        #           [    3, 41455],
-        #           [    4, 45470],
-        #           [    5, 45325],
-        #           [    5, 46390]])
-        # Each row is a (gt index, prediction index)
-        # Note how gt items 1, 2, 3, and 5 each have two ties
 
         pred_inds_to_update = gt_pred_pairs_of_highest_quality[1]
         matches[pred_inds_to_update] = all_matches[pred_inds_to_update]
+
+        
 
 def overwrite_eps(model, eps):
     """
